c656515eaf1bda37fc84ca36c879136d0afb596c
Ramya Iyer <ramyaiyer1000@gmail.com>
2024-12-04 17:37:40 -0800

    lin reg model and updated features

--- lstm.py
+++ lstm.py
@@ -3,7 +3,6 @@ Define LSTM Model
 """
 import torch.nn as nn
 
-
 class LSTMRegressor(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(LSTMRegressor, self).__init__()
